a82784c5d34c5dc75a2a7ae136d3392daa04e823
linzetian <65435639+linzetian@users.noreply.github.com>
2021-03-01 15:00:46 +0800

    Update 2021-3-1通过掩膜提高图片对比度.cpp

--- 2021-3-1通过掩膜提高图片对比度.cpp
+++ 2021-3-1通过掩膜提高图片对比度.cpp
@@ -18,11 +18,11 @@ int main()
 	imshow("p1", src);
 
 
-	int cols = (src.cols - 1) * src.channels();
+	int cols = (src.cols -1) * src.channels();//为什么src.cols-1,直接不用减一好像也可以实现（因为例如9列，即0-8）
 	int offsetx = src.channels();
 	int rows = src.rows;
 	//掩膜操作后，提高对比度
-	dst = Mat::zeros(src.size(), src.type());//关于dat的定义问题，此处是否可以改进
+	dst =Mat::zeros(src.size(), src.type());//关于dat的定义问题，此处是否可以改进//zeros初始化定义dst必须与输入的图像大小，类型一致
 	int row = 0;
 	int col = 0;
 	for (row = 1; row < (rows - 1); row++)
@@ -30,7 +30,7 @@ int main()
 		const uchar* previous = src.ptr<uchar>(row - 1);
 		const uchar* current = src.ptr<uchar>(row);
 		const uchar* next = src.ptr<uchar>(row + 1);
-		uchar* output = dst.ptr<uchar>(row);
+		uchar* output = dst.ptr<uchar>(row);//还需要理解const使用场合
 		for (col = offsetx; col < cols; col++)
 		{
 			output[col] = saturate_cast<uchar>(5 * current[col] - (current[col - offsetx] + current[col + offsetx] + previous[col] + next[col]));
@@ -40,4 +40,3 @@ int main()
 	imshow("output", dst);
 	waitKey(0);
 	return 0;
-}
